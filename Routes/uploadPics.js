4c572cb06a3c3b3c3bd95806e479c903ad169ef8
Olaide Abiola <olaideabiola2@gmail.com>
2023-10-06 21:32:47 +0100

    added

--- Routes/uploadPics.js
+++ Routes/uploadPics.js
@@ -1,33 +1,45 @@
-const express = require("express");
-const router = express.Router();
-const multer = require("multer");
-const upload = multer({dest: "public/"});
-const { taskCollection } = require("../schema/taskSchema");
-const { isUserLoggedIn } = require("./middlewares");
+// const express = require("express");
+// const router = express.Router();
+// const multer = require("multer");
+// const upload = multer({dest: "public/"});
+// const { taskCollection } = require("../schema/taskSchema");
+// const { isUserLoggedIn } = require("./middlewares");
 
-router.use(isUserLoggedIn);
+// router.use(isUserLoggedIn);
 
-router.post("/pic", upload.single("file"), async (req, res) => {
 
-    try {
-        const {taskTitle, taskBody} = req.body;
-    const {originalname} = req.file;
-    const {userId} = req.decoded;
+
+// module.exports = router;
+// // 1
+// const express = require("express");
+// const router = express.Router();
+// const multer = require("multer");
+// const upload = multer({dest: "public/"});
+// const { taskCollection } = require("../schema/taskSchema");
+// const { isUserLoggedIn } = require("./middlewares");
+
+// router.use(isUserLoggedIn);
+
+// router.post("/pic", upload.single("file"), async (req, res) => {
+
+//     try {
+//         const {taskTitle, taskBody} = req.body; 
+//     const {originalname} = req.file;
+//     const {userId} = req.decoded;
     
 
-    const newTask = await taskCollection.create({
-        taskTitle, taskBody, pictureName: originalname, user: userId
-    });
+//     const newTask = await taskCollection.create({
+//         taskTitle, taskBody, pictureName: originalname, user: userId
+//     });
 
-    res.send({
-        successful: true,
-        newTask
-    });
+//     res.send({
+//         successful: true,
+//         newTask
+//     });
 
-    } catch (error) {
-        res.status(500).json({message: "Internal server error"});
-    }
-});
+//     } catch (error) {
+//         res.status(500).json({message: "Internal server error"});
+//     }
+// });
 
-module.exports = router;
-1
+// module.exports = router;
--- index.js
+++ index.js
@@ -41,18 +41,18 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/v1/tasks", taskRoute);
 app.use("/v1/auth", authRoute);
 
-app.post("/pic", upload.single("filename"), async (req, res) => {
+app.post("/pic", upload.single("file"), async (req, res) => {
 
     try {
         const {taskTitle, taskBody} = req.body;
-    const {originalname} = req.file;
+    const {filename} = req.file;
     const {userId} = req.decoded;
   
     console.log(req.file);
-    cloudinary.uploader.upload({})
-  
+    const cloudinaryUpload = await cloudinary.uploader.upload("public/", + filename);
+        console.log(cloudinaryUpload);
     const newTask = await taskCollection.create({
-        taskTitle, taskBody, pictureName: originalname, user: userId
+        taskTitle, taskBody, pictureName: filename, user: userId
     });
   
     res.send({
@@ -65,6 +65,66 @@ app.post("/pic", upload.single("filename"), async (req, res) => {
     }
   });
 
-//app.use("/v1/upload-pic", uploadPic);
+// //app.use("/v1/upload-pic", uploadPic);
+// const express = require("express");
+// const port = process.env.PORT || 3000;
+// const app = express();
+// const monogoose = require("mongoose");
+// require("dotenv").config();
+// const tasksRoute = require("./Routes/tasks");
+// const authRoute = require("./Routes/auth");
+// const path = require("path");
+// // const taskWithPicture = require("./routes/uploadPics");
 
 
+// const multer = require("multer");
+// const upload = multer({dest: "public/"});
+// const { taskCollection } = require("./schema/taskSchema");
+// const { isUserLoggedIn } = require("./Routes/middleware");
+
+// const connect = monogoose.connect(process.env.mongoDBURL);
+
+// connect.then(() => {
+//   console.log("Connected sucessfully to my database");
+// }).catch((error) => {
+//   console.log("Could not connect to the database, reason =", error);
+// });
+
+
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.use("/v1/tasks", tasksRoute);
+// app.use("/v1/auth", authRoute);
+// // app.use("/v1/upload-pic", taskWithPicture)
+
+// app.use(isUserLoggedIn);
+
+// app.post("/pic", upload.single("file"), async (req, res) => {
+
+//   try {
+//       const {taskTitle, taskBody} = req.body;
+//   const {filename} = req.file;
+//   const {userId} = req.decoded;
+
+//   console.log(req.file);
+  
+
+//   const newTask = await taskCollection.create({
+//       taskTitle, taskBody, pictureName: filename, user: userId
+//   });
+
+//   res.json({
+//       successful: true,
+//       newTask
+//   });
+
+//   } catch (error) {
+//       res.status(500).json({message: "Internal server error"});
+//   }
+// });
+
+// app.listen(port, function() {
+//   console.log("Listening on port", port);
+// });
